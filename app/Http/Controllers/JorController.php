0ba1a575ca6186103f9edc7e24787472dbc74986
jorge <cyrogonzalez623@gmail.com>
2026-06-18 20:45:51 -0300

    hola

---
+++ app/Http/Controllers/JorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jor;
+use App\Http\Requests\StoreJorRequest;
+use App\Http\Requests\UpdateJorRequest;
+
+class JorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jor $jor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jor $jor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJorRequest $request, Jor $jor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jor $jor)
+    {
+        //
+    }
+}
