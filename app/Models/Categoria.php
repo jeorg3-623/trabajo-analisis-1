5818d154e1e14d092ca89afd9eeb37b0f80a6054
unknown <guillermodxd49@gmail.com>
2026-06-26 13:52:32 -0300

    lkajdflkasndf

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Producto;
 
-class Categorias extends Model
+class Categoria extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoriasFactory> */
     use HasFactory;
